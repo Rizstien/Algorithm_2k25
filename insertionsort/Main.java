0938000db2edccc1f8f504d7cc5b9e00937b70eb
Rizwan Munir <rizwan.munir@infocirrus.com>
2025-01-10 03:51:47 +0500

    insertion sort

---
+++ insertionsort/Main.java
@@ -0,0 +1,27 @@
+package insertionsort;
+
+public class Main{
+    public static void main(String[] args) {
+        int[] arr = {12,11,13,5,6};
+        insertSort(arr);
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+
+    private static int[] insertSort(int[] arr){
+        for(int i = 1; i<arr.length; i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        return arr;
+    }
+}
+
+
