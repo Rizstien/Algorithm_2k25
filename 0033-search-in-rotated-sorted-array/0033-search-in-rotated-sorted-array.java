90ba4215436c512021a4763ffaee129f293837e1
Rizwan Munir <rizstien@gmail.com>
2025-01-16 23:04:51 +0500

    Time: 0 ms (100%), Space: 42.1 MB (49.84%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return search(nums, 0,nums.length-1, target);
+        return search(nums, 0, nums.length-1, target);
     }
 
-    public int search(int[] nums, int start, int end, int target){
-        int mid = start + (end-start)/2;
+    public int search(int[] nums, int left, int right, int target){
+        int mid = right + (left - right)/2;
 
-        if(start > end) return -1;
+        if(left > right) return -1;
 
-        if(target == nums[mid]) return mid;
-        else if(nums[start] <= nums[mid]){
-            if(target >= nums[start] && target < nums[mid]){
-                end = mid - 1;
+        if(nums[mid] == target) return mid;
+        else if(nums[left] <= nums[mid]){
+            if(target >= nums[left] && target < nums[mid]){
+                right = mid - 1;
             }else{
-                start = mid + 1;
+                left = mid + 1;
             }
-        }else {
-            if(nums[mid] < target && target <= nums[end]){
-                start = mid + 1;
+        }else{
+            if(target <= nums[right] && target > nums[mid]){
+                left = mid + 1;
             }else{
-                end = mid - 1;
+                right = mid - 1;
             }
         }
-        return search(nums, start, end, target);
+
+        return search(nums, left, right, target);
     }
 }
