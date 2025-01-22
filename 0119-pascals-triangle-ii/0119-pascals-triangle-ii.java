96bd1a42278ea1bfa61ea9941ea768d91aaf7380
Rizwan Munir <rizstien@gmail.com>
2025-01-22 09:10:34 +0500

    Time: 0 ms (100%), Space: 40.9 MB (88.32%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,31 +1,13 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> result = generate(rowIndex+1);
-        return result.get(rowIndex);
-    }
-
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-        for(int i=0;i<numRows;i++){
-
-            if(i==0) {
-                result.add(new ArrayList<Integer>(Arrays.asList(1)));
-                continue;
-            }else if(i==1) {
-                List<Integer> currentRow = new ArrayList<Integer>(Arrays.asList(1));
-                currentRow.add(1);
-                result.add(currentRow);
-                continue;
-            }else{
-                List<Integer> currentRow = new ArrayList<Integer>(Arrays.asList(1));
-                List<Integer> prevRow = result.get(i-1);
-                for(int j=1;j<i;j++){
-                    currentRow.add(prevRow.get(j)+prevRow.get(j-1));
-                }
-                currentRow.add(1);
-                result.add(currentRow);
-            }
+        List<Integer> row = new ArrayList<>();
+        long value = 1; // Starting value for C(n, 0)
+        row.add((int) value);
+        
+        for (int k = 1; k <= rowIndex; k++) {
+            value = value * (rowIndex - k + 1) / k; // C(n, k) = C(n, k-1) * (n-k+1)/k
+            row.add((int) value);
         }
-        return result;
+        return row;
     }
 }
