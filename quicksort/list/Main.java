cb39b6519568b45c6b9fd39bb41b4d342cc52640
Rizwan Munir <rizstien@gmail.com>
2025-04-06 16:55:12 +0500

    quick sort algos

---
+++ quicksort/list/Main.java
@@ -0,0 +1,82 @@
+package quicksort.list;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(12);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(13);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(15);
+
+        Main instance = new Main();
+        instance.sortList(head);
+        instance.print(head);
+    }
+
+    public ListNode sortList(ListNode head) {
+        ListNode tail = getTail(head);
+        quickSort(head, tail);
+        return head;
+    }
+
+    public ListNode getTail(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode start = head;
+        while(start != null && start.next != null){
+            start = start.next;
+        }
+        return start;
+    }
+
+    public void quickSort(ListNode start, ListNode end){
+        if(start != null && start != end && start != end.next ){
+            ListNode pivot = partition(start, end);
+            quickSort(start, getPrev(start, pivot));
+            quickSort(pivot.next, end);
+        }
+    }
+
+    public ListNode partition(ListNode start, ListNode end){
+        int pivot = end.val;
+        ListNode pIndex = start;
+        ListNode curr = start;
+
+        while(curr != end){
+            if(curr.val <= pivot){
+                int temp = curr.val;
+                curr.val = pIndex.val;
+                pIndex.val = temp;
+
+                pIndex = pIndex.next;
+            }
+            curr = curr.next;
+        }
+
+        int temp = pIndex.val;
+        pIndex.val = end.val;
+        end.val = temp;
+
+        return pIndex;
+    }
+
+    public ListNode getPrev(ListNode start, ListNode target){
+        while(start != target && start.next != target){
+            start = start.next;
+        }
+        return start;
+    }
+
+    public void print(ListNode head){
+        while(head != null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
---
+++ quicksort/lomutopartition/Main.java
@@ -0,0 +1,43 @@
+package quicksort.lomutopartition;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {11,12,13,5,6};
+        quickSort(arr, 0, arr.length-1);
+        Arrays.stream(arr).forEach(i-> System.out.println(i));
+    }
+
+    private static void quickSort(int[] arr, int low, int high){
+        if(low<high){  // run this untill the low index ramins in bound and stayes less then high index
+            int pivot = lomutoPartition(arr, low, high);  //find pivot index
+            quickSort(arr, low, pivot-1); // sort left hald of array untill before pivot
+            quickSort(arr, pivot+1, high); // sort right half of array just after pivot
+            // the pivot value is already in right position during finding pivot operation
+        }
+    }
+
+    private static int lomutoPartition(int[] arr, int low, int high){
+        int pivot = arr[high];  // as lumoto partitioning formula assume the highest element as pivot
+        int i = low-1; // this keeps the index of last element that is smaller or equal to pivot
+
+        for(int j=low; j<high; j++){
+            if(arr[j] < pivot){   // the element at i index should be smaller then pivot not j so swap their positions
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i+1, high);   // now put the pivot at right position that is after the one smaller element then pivot
+        return i+1; // return pivot position
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+
