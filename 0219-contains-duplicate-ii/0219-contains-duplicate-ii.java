6712396c71a5259716a35bd9b4b5bdee45b98403
Rizwan Munir <rizstien@gmail.com>
2025-10-02 08:37:39 +0500

    Time: 24 ms (27.15%), Space: 60 MB (27.97%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,17 +1,17 @@
 class Solution {
-    // Make a HashMap whos key is the num array elements and whose value is the uindex of array
-    Map<Integer, Integer> valueIndex = new HashMap<>();
+    // Make a HashMap whos key is the num array elements and whose value is the index of array
+    Map<Integer, Integer> valueIndex = new HashMap<>(); //O(n) space complexity
     public boolean containsNearbyDuplicate(int[] nums, int k) {    
-        for(int i=0;i<nums.length;i++){
-            if(valueIndex.containsKey(nums[i]) && checkDistance(nums, i, k)) {
-                 return true;    
-            }
+        for(int i=0;i<nums.length;i++){   //O(n)  time complexity
+            // check if the value already exist in map then what was its previous index and verify with new duplicate value index if it satisfys the condition  then return true. Otherwise add/replce the value index in map so it replces with new index for newxt time condition check
+            if(valueIndex.containsKey(nums[i]) && checkDistance(nums, i, k)) return true;    
             else valueIndex.put(nums[i],i);
         }
         // No duplicates found or no matching condition abs(i-j) <= k found
         return false; 
     }
 
+    //  check if the distance between duplicate value indexesis satisfying condition
     private boolean checkDistance(int[] nums, int i, int k){
         int indexi = valueIndex.get(nums[i]);
         int indexj = i;
