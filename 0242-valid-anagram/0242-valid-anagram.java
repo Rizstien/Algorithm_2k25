0c21dee114538871b06b69c38cea697658ad1b7f
Rizwan Munir <rizstien@gmail.com>
2025-10-02 20:45:56 +0500

    Time: 3 ms (90.5%), Space: 43.4 MB (63.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,25 +3,27 @@ class Solution {
         if(s.length()!=t.length()) return false;
         
         // make an array of size 26 and fill with zeros to track the frequency of each charcter in both strings
-        int[] freq = new int[26];
-        Arrays.fill(freq, 0);
+        int[] freq = new int[26];   //O(1) space complexity
+        Arrays.fill(freq, 0);       // O(1)  time complexity
 
         char[] sArr = s.toCharArray();
         char[] tArr = t.toCharArray();
         
         // since both string are same length so just one iterator will be enough for both arrays iteration
-        for(int i=0;i<sArr.length;i++){
+        for(int i=0;i<sArr.length;i++){    //  O(n)  time complexity
             freq[sArr[i] - 'a']++;  // just plus one at same index as of charcter in freq array
             freq[tArr[i] - 'a']--; // just minus one at same index as of charcter in freq array
         }
         // at the end of this iteration if the strings are anagrams and have same charcters wirth same frequency they should have cancel each other frequency
 
         // at the end just check if the whole array is not all 0 then they are not anagram
-        for(int i=0;i<freq.length;i++){
+        for(int i=0;i<freq.length;i++){   // O(1)   time complexity
             if(freq[i]!=0) return false;
         }
-
         return true;
-
     }
+
+        // Overall O(n) time complexity
+
+
 }
