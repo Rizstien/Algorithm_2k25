cb306bbcaf64b805a61e276fa617cd99a0548764
Rizwan Munir <rizstien@gmail.com>
2025-10-05 17:38:17 +0500

    Time: 7 ms (64.04%), Space: 47.9 MB (70.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-         Map<String, List<String>>  map = new HashMap<>();
-         for(String str: strs){
+        // Make a hashmap to hold all the anagrams against a similar pattren
+         Map<String, List<String>>  map = new HashMap<>(); //O(n)  space
+         // Iterate over all strings array 
+         for(String str: strs){     // O(n)  time
             char[] charArr = str.toCharArray();
-            Arrays.sort(charArr);
+            Arrays.sort(charArr);   // O(kLogk)
             String key = String.valueOf(charArr);
             if(!map.containsKey(key)){
-                map.put(key, new ArrayList<>());
+                map.put(key, new ArrayList<>());   // O(1)
             }
-            map.get(key).add(str);
+            map.get(key).add(str);  // O(1)
          }
          return new ArrayList<>(map.values());
     }
+
+
+    // Overall O(n) space complexity  +  O(n x kLogk)  =  O(N*K) 
+
 }
