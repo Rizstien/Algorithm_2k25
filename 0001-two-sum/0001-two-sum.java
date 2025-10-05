a8d2ed625a54f8994c87b9c0df5a16d360c1cde8
Rizwan Munir <rizstien@gmail.com>
2025-10-05 19:23:01 +0500

    Time: 2 ms (98.92%), Space: 45.1 MB (32.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer>  map = new HashMap<>();
-        int[] result = new int[2];
 
         // Iterate in O(n) and check if the remaing value is present in map O(1)
         for(int i=0;i<nums.length;i++){
             int value = nums[i];
-            result[0] = i;
             if(map.containsKey(target-value)){
-                result[1] = map.get(target-value);
-                break;
+                return new int[]{map.get(target-value), i};
             }else{
                 map.put(value, i); 
             }
         }
-        return result;
+        return new int[]{};
     }
 }
