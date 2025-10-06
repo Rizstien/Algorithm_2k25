69c81c861bcaad7eaf82fc369302ff0cf7e0b8f1
Rizwan Munir <rizstien@gmail.com>
2025-10-06 06:24:41 +0500

    Time: 6 ms (98.69%), Space: 48.3 MB (29.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,20 +3,10 @@ class Solution {
         // Make a hashmap to hold all the anagrams against a similar pattren
          Map<String, List<String>>  map = new HashMap<>(); //O(n)  space
          // Iterate over all strings array 
-         for(String str: strs){   // O(n)  time
-            
-            int[] count = new int[26];
-            for(char c : str.toCharArray()){  //O(k) time
-                count[c-'a']++;
-            }
-            
-            StringBuilder sb = new StringBuilder("");
-            for(int i=0;i<26;i++){ //O(1)
-                sb.append("#");
-                sb.append(count[i]);
-            }
-            String key = sb.toString();
-            
+         for(String str: strs){     // O(n)  time
+            char[] charArr = str.toCharArray();
+            Arrays.sort(charArr);   // O(kLogk)
+            String key = String.valueOf(charArr);
             if(!map.containsKey(key)){
                 map.put(key, new ArrayList<>());   // O(1)
             }
@@ -26,6 +16,6 @@ class Solution {
     }
 
 
-    // Overall O(n) space complexity  +  O(n x k) time complexity
+    // Overall O(n) space complexity  +  O(n x kLogk)  =  O(N*K) since O(Klogk) is almost O(K)
 
 }
