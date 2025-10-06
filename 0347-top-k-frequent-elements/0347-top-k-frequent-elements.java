f53d95b3bc656b57f281198582273ac7a5cf443a
Rizwan Munir <rizstien@gmail.com>
2025-10-06 12:47:43 +0500

    Time: 16 ms (19.19%), Space: 48.8 MB (43.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -11,18 +11,19 @@ class Solution {
             freq.put(num, freq.getOrDefault(num,0)+1);
         }
 
-        // 
+        // make a custom heap with comparator that add that keep the high frequncy values on priority
         Queue<Integer> heap = new PriorityQueue<>(
             (a, b) -> freq.get(a) - freq.get(b)
         );
-        
+
+        // iterate through all the keyset of freq map and put in heap based on frequency
         for(int num : freq.keySet()){
             heap.add(num);
-            if(heap.size()>k) heap.poll();
+            if(heap.size()>k) heap.poll();  // no need to keep more then k elements to save space so poll the low freq elements
         }
 
         for(int i=0;i<k;i++){
-            result[i] = heap.poll();
+            result[i] = heap.poll();    // build the result array from heap
         }
 
         return result;
