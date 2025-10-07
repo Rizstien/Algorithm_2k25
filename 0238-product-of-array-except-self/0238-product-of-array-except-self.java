06edd1364cbddf71242737b860c829b5ec28831a
Rizwan Munir <rizstien@gmail.com>
2025-10-07 07:07:59 +0500

    Time: 2 ms (87.34%), Space: 55.4 MB (75.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,33 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
-        int[]  pre = new int[nums.length];
-        int[]  post = new int[nums.length];
         int[]  ans = new int[nums.length];
+        Arrays.fill(ans, 1);
 
-        // Populate a prefix product array for every element and assume that if there is no prefix to an element then it is 1
-        int prevProd = 1;
+        int pre = 1, post = 1;
         for(int i=0;i<nums.length;i++){
-            if(i-1<0){
-                pre[i] = 1;
-            } else{
-                pre[i] = nums[i-1]*pre[i-1];
-            } 
+            ans[i] = pre;
+            pre = nums[i]*pre;
         }
 
-        // Populate a postfix product array for every element and assume that if there is no postfix to an element then it is 1
-        int postProd = 1;
         for(int i=nums.length-1;i>=0;i--){
-            if(i+1>nums.length-1){
-                post[i] = 1;
-            } else{
-                post[i] = nums[i+1]*post[i+1];
-            } 
-        }
-
-        // multiply both array and that is answer
-        for(int i=0;i<nums.length;i++){
-            ans[i] = pre[i]*post[i];
+            ans[i] = post*ans[i];
+            post = nums[i]*post;
         }
 
         return ans;
