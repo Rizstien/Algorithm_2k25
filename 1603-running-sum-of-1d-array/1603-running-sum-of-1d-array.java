a6096fdee2745d56d4563add6c1cdbf299949025
Rizwan Munir <rizstien@gmail.com>
2025-10-08 06:48:15 +0500

    Time: 0 ms (100%), Space: 42.8 MB (41.52%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         int preSum = 0;
-        for(int i=0; i<nums.length;i++){
+        for(int i=0; i<nums.length; i++){
         // Take current value and add previous Sum and replace the current value
-            nums[i] = preSum + nums[i]; 
+            nums[i] += preSum; 
         // new preSum will be the current sum calculated and it will be used in next iteration
             preSum = nums[i];
         }
