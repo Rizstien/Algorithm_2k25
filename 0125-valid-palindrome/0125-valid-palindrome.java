1631da80273dabcf3d9503577fb2179f9e1f726c
Rizwan Munir <rizstien@gmail.com>
2025-10-11 22:59:11 +0500

    Time: 2 ms (98.61%), Space: 43.3 MB (62.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
- 
- /*
+        
+        /*
         HashSet<Character> charSet = new HashSet<Character>();
         for(char c='a';c<='z';c++){
             charSet.add(c);
@@ -20,18 +20,22 @@ class Solution {
 
 */
 
-
         int i=0,j=s.length()-1;
-        while(i<j){
-            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+        while(i<=j){
+            if(!Character.isLetterOrDigit(s.charAt(i))) { 
                 i++;
+                continue;
             }
-            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+            if(!Character.isLetterOrDigit(s.charAt(j))) {
                 j--;
+                continue;
             }
-            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
-            i++;
-            j--;
+            if(Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(j))){
+                i++;
+                j--;
+            }else{
+                return false;
+            }
         }
 
         return true;
