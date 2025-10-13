9e2928d628a6e6f0c16ba142c00ad9a1018c912a
Rizwan Munir <rizstien@gmail.com>
2025-10-13 07:21:53 +0500

    Time: 1 ms (60.08%), Space: 45.9 MB (89.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -26,10 +26,10 @@ class Solution {
         */
 
         //Single Pass approach
+        /*
         int total=0;
         int[] leftMax = new int[height.length];
         int[] rightMax = new int[height.length];
-        //int[] total = new int[height.length];
         
         int leftMaxHeight=0,rightMaxHeight=0,water=0;
         for(int left = height.length-1;left>=0;left--){
@@ -45,6 +45,29 @@ class Solution {
         for(int i = 0;i<height.length;i++){
             water = Math.min(rightMax[i],leftMax[i]) - height[i];
             total += water;
+        }
+        return total;
+        */
+
+        //Optimal Solution with 2 Ptrs - O(n) time and O(1) space
+        
+        int leftMax=0,rightMax=0,total=0;
+        int left=0,right=height.length-1;
+
+        while(left<right){
+            if(height[left] < height[right]){
+                leftMax = Math.max(leftMax, height[left]);
+                total = leftMax - height[left] > 0 ?
+                    total + leftMax - height[left] : 
+                    total;
+                left++;
+            }else if(height[right] <= height[left]){
+                rightMax = Math.max(rightMax, height[right]);
+                total = rightMax - height[right] > 0 ?
+                    total + rightMax - height[right] : 
+                    total;
+                    right--;
+            }
         }
         return total;
     }
