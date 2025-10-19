7f39d3cf8f994daee1ed4a9699d109455e4dc1bb
Rizwan Munir <rizstien@gmail.com>
2025-10-19 15:01:59 +0500

    Time: 6 ms (83.82%), Space: 42.7 MB (81.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if(s2.length()<s1.length()) return false;
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
         for(char c : s1.toCharArray()){
@@ -9,16 +10,16 @@ class Solution {
         int l=0;
         int r=l+s1.length()-1;
 
-        while(l<=r && r<s2.length()){
-            freq2 = new int[26];
-            for(int i=l;i<=r;i++){
-                freq2[s2.charAt(i)-'a']++;
-            }
+        for(int i=l;i<=r;i++){
+            freq2[s2.charAt(i)-'a']++;
+        }
 
+        while(l<=r && r<s2.length()){
             if(Arrays.equals(freq1, freq2)) return true;
             else{
-                l++;
-                r++;
+                freq2[s2.charAt(l++)-'a']--;
+                if(++r<s2.length())
+                    freq2[s2.charAt(r)-'a']++;
             }
         }
         return false;
