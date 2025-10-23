3643e67b6289a447d6325fc262e1a10f21eef35e
Rizwan Munir <rizstien@gmail.com>
2025-10-23 07:42:11 +0500

    Time: 33 ms (38.43%), Space: 58.4 MB (80.15%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // B.F approach O(n*k)
+/*
+        int[] msw = new int[nums.length-k+1];
+        int l=0,r=l+k-1;
+        while(r<nums.length){
+            int max = nums[l];
+            for(int i=l;i<=r;i++){
+                max = Math.max(max, nums[i]);
+            }
+            msw[l] = max;
+            l++;r++;
+        }
+        return msw;
+*/
+
+        // Optimal Solution O(n) space + O(k) time
+
+        int[] msw = new int[nums.length-k+1];
+        int l=0,r=l+k-1;
+        Deque<Integer> deque = new LinkedList<>();
+        for(int i=0;i<nums.length;i++){
+            while(!deque.isEmpty() && deque.peek() < i-k+1){
+                deque.poll();
+            }
+            while(!deque.isEmpty() && nums[deque.peekLast()] < nums[i]){
+                deque.pollLast();
+            }
+            deque.offer(i);
+            if(i >= k-1){
+                msw[i-k+1] = nums[deque.peek()];
+            }
+        }
+        return msw;
+    }
+}
