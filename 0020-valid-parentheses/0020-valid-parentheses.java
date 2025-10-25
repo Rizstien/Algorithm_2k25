94bf34c791da9339bd9c44f092da57b325d884c8
Rizwan Munir <rizstien@gmail.com>
2025-10-25 07:28:57 +0500

    Time: 5 ms (11.94%), Space: 42.1 MB (39.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
         if(s.length() == 1) return false;
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+
         Stack<Character> stack = new Stack<>();
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-            if(c == '(' || c == '{' || c == '['){
+            if(!map.containsKey(c)){
                 stack.push(c);
-            }
-            else if(c == ')' || c == '}' || c == ']'){
+            }else {
                 if(stack.empty()) return false;
                 char openingBracket = stack.pop();
-                if(c == ')' && openingBracket != '(') return false;
-                else if(c == '}' && openingBracket != '{') return false;
-                else if(c == ']' && openingBracket != '[') return false;
+                if(map.get(c) != openingBracket) return false;
             }
         }
         if(stack.empty()) return true;
