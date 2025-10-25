5a332f48274fc54ac9040c781503bdabdc0f80eb
Rizwan Munir <rizstien@gmail.com>
2025-10-25 06:51:06 +0500

    Time: 19 ms (50.17%), Space: 45.3 MB (47.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,36 +1,43 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length() == 0 || t.length() == 0 || s.length() < t.length()) return "";
+        if(s.length() < t.length())
+            return "";
 
-        Map<Character, Integer> tMap = new HashMap<Character, Integer>();
-        for(char c : t.toCharArray()){
-            tMap.put(c, tMap.getOrDefault(c,0)+1);
+        Map<Character,Integer> tMap = new HashMap<>();
+        Map<Character,Integer> subStringMap = new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i),0)+1);
         }
-
-        Map<Character, Integer> subStringMap = new HashMap<Character, Integer>();
-        int r=0,l=0;
-        int[] ans = {-1, 0, 0};
+        int[] ans = {-1,0,0};
+        int l=0,r=0;
         int required = tMap.size();
         int created = 0;
 
-        while(r < s.length()){
+        while(r<s.length()){
             char c = s.charAt(r);
-            subStringMap.put(c, subStringMap.getOrDefault(c,0) + 1);
-            if(tMap.containsKey(c) && tMap.get(c).intValue() == subStringMap.get(c).intValue()) created++;
-            while(l<=r && required == created){
+            subStringMap.put(c,subStringMap.getOrDefault(c,0)+1);
+            if(tMap.containsKey(c) && subStringMap.get(c).intValue() == tMap.get(c).intValue())
+                created++;
+            
+            while(l<=r && created == required){
                 c = s.charAt(l);
-                if(ans[0] == -1 || ans[0] > r - l + 1){
-                    ans[0] = r - l +1;
+                if(ans[0] == -1 || ans[0] > r-l+1){
+                    ans[0] = r-l+1;
                     ans[1] = l;
                     ans[2] = r;
                 }
-                subStringMap.put(c, subStringMap.get(c) - 1);
-                if(tMap.containsKey(c) && subStringMap.get(c).intValue() < tMap.get(c).intValue()) created--;
                 l++;
+                subStringMap.put(c,subStringMap.getOrDefault(c,0)-1);
+                if(tMap.containsKey(c) && subStringMap.get(c) < tMap.get(c))
+                    created--;
             }
             r++;
         }
         if(ans[0] == -1) return "";
         return s.substring(ans[1], ans[2]+1);
+
+
+
+
     }
 }
