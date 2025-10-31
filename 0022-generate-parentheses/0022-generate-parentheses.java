d6a24da7ff57a23709756bc6773f0ee2346b2936
Rizwan Munir <rizstien@gmail.com>
2025-10-31 14:16:59 +0500

    Time: 0 ms (100%), Space: 44.5 MB (8.1%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<String>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+
+    // rule 1: for every valid ans, it should start with a opening bracket
+    // rule 2: at any moment the closing brackets can be equal or less then the opening brackets
+    // rule3 3: we can only add a closing bracket if there is a opening bracket before that
+    public void backtrack(List<String> ans, StringBuilder curr, int open, int close, int n){
+        if(curr.length()==n*2){
+            ans.add(curr.toString());
+            return;
+        }
+
+        if(open<n){
+            curr.append("(");
+            backtrack(ans, curr, open+1, close, n);
+            curr.deleteCharAt(curr.length()-1);
+        }
+
+        if(close<open){
+            curr.append(")");
+            backtrack(ans, curr, open, close+1, n);
+            curr.deleteCharAt(curr.length()-1);
+        }
+    }
+}
