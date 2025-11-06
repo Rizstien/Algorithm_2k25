a7e01f97da04bb14e9532b7c488945fbd93c38cd
Rizwan Munir <rizstien@gmail.com>
2025-11-06 20:38:03 +0500

    Time: 0 ms (100%), Space: 44 MB (5.09%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,5 +1,25 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+
+        int top = 0;
+        int bottom = matrix.length-1;
+
+        while(top<=bottom){
+            int mid = top + (bottom-top)/2;
+            if(matrix[mid][0]==target || matrix[mid][matrix[mid].length-1]==target) return true;
+
+            else if(matrix[mid][0] < target && matrix[mid][matrix[mid].length-1] > target){
+                if(binarySearch(matrix[mid], target)) return true;
+                else return false;
+            }else if(matrix[mid][0] < target && matrix[mid][matrix[mid].length-1] < target){
+                top = mid+1;
+            }else{
+                bottom = mid-1;
+            }
+
+        }
+
+/*
         for(int[] nums : matrix){
             if(nums[0]==target || nums[nums.length-1]==target) return true;
             else if(nums[0] < target && nums[nums.length-1] > target){
@@ -7,6 +27,7 @@ class Solution {
                 else return false;
             }
         }
+        */
         return false;
     }
 
