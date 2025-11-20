1c852739d34f96e06e47facee6d970e17bdbc6f8
Rizwan Munir <rizstien@gmail.com>
2025-11-20 15:39:18 +0500

    Time: 0 ms (100%), Space: 43.1 MB (28.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -7,34 +7,23 @@ class Solution {
             return;
         }
 
-
-        int[] result = new int[m+n];
-        int i=0,j=0,k=0;
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                result[k] = nums1[i];
-                i++;
-                k++;
+       // int[] result = new int[m+n];
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k--] = nums1[i--];
             }else{
-                result[k] = nums2[j];
-                j++;
-                k++;
+                nums1[k--] = nums2[j--];
             }
         }
 
-        while(j<n){
-            result[k++] = nums2[j++];
+        while(j>=0){
+            nums1[k--] = nums2[j--];
         }
 
-        while(i<m){
-            result[k++] = nums1[i++];
+        while(i>=0){
+            nums1[k--] = nums1[i--];
         }
-
-        for(int l=0;l<m+n;l++){
-            nums1[l] = result[l];
-        }
-
-        
-        
+     
     }
 }
