f8ff64e9a16f17840b1ec150348ac1d2ce99b2bb
Rizwan Munir <rizstien@gmail.com>
2025-11-21 16:19:12 +0500

    Time: 1 ms (58.92%), Space: 43.2 MB (14.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -2,6 +2,7 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
 
         //B.F
+        /*
         if(strs.length==1) return strs[0];
         String first = strs[0];
         StringBuilder sb = new StringBuilder();
@@ -15,5 +16,22 @@ class Solution {
             sb.append(c);
         }
         return sb.toString();
+        */
+
+        if (strs.length == 0) return "";
+
+        Arrays.sort(strs);
+
+        String first = strs[0];
+        String last  = strs[strs.length - 1];
+
+        int i = 0;
+        while (i < first.length() 
+                && i < last.length() 
+                && first.charAt(i) == last.charAt(i)) {
+            i++;
+        }
+
+        return first.substring(0, i);
     }
 }
