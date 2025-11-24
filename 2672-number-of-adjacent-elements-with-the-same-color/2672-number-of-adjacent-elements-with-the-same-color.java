b9d3e247c728098752b4315770a53edf4290129b
Rizwan Munir <rizstien@gmail.com>
2025-11-24 16:23:46 +0500

    Time: 4 ms (100%), Space: 208 MB (26.65%) - LeetHub

--- 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.java
+++ 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.java
@@ -3,29 +3,32 @@ class Solution {
 
         int[] result = new int[queries.length];
         int[] colors = new int[n];
-
         int same = 0;
+
         for(int i=0;i<queries.length;i++){
             int index = queries[i][0];
             int newColor = queries[i][1];
             int oldColor = colors[index];
 
+            // remove old contributions
             if(oldColor != 0){
-                // remove old contributions
-                if(index>0 && oldColor == colors[index-1]) same--;
-                if(index<n-1 && oldColor == colors[index+1]) same--;
+                if(index>0 && colors[index-1] == oldColor) same--;
+                if(index<n-1 && colors[index+1] == oldColor) same--;
             }
 
+            // update color
             colors[index] = newColor;
 
+            // update new contributions
             if(newColor != 0){
-                // add new contributions
-                if(index>0 && newColor == colors[index-1]) same++;
-                if(index<n-1 &&  newColor == colors[index+1]) same++;
+                if(index>0 && colors[index-1] == newColor) same++;
+                if(index<n-1 && colors[index+1] == newColor) same++;
             }
+
             result[i] = same;
         }
 
         return result;
+        
     }
 }
