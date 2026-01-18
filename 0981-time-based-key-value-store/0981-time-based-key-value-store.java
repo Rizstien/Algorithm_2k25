2a2d64cb4736c4b5cf41e8417dd81856d94f91bd
Rizwan Munir <rizstien@gmail.com>
2026-01-18 20:10:59 +0500

    Time: 154 ms (29.78%), Space: 108.7 MB (41.06%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,43 +1,20 @@
 class TimeMap {
 
-    Map<String, TreeMap<Integer, String>> map = null;
+    private Map<String, TreeMap<Integer, String>> map = null;
     public TimeMap() {
-        map = new HashMap<>();
+        map = new HashMap<>(); 
     }
     
     public void set(String key, String value, int timestamp) {
-        /*
-        if(map.get(key) != null){
-            TreeMap<Integer, String> interMap = map.get(key);
-            interMap.put(timestamp, value);
-        }else{
-            TreeMap<Integer, String> interMap = new TreeMap<>(Collections.reverseOrder());
-            interMap.put(timestamp,value);
-            map.put(key,interMap);
-        }
-        */
-
-        map.computeIfAbsent(key, k-> new TreeMap<>()).put(timestamp,value);
+        map.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
 
     }
     
     public String get(String key, int timestamp) {
         TreeMap<Integer, String> treeMap = map.get(key);
         if(treeMap == null) return "";
-        
         Map.Entry<Integer, String> entry = treeMap.floorEntry(timestamp);
-
         return entry == null ? "" : entry.getValue();
-
-        /*
-        if(map.get(key).get(timestamp) != null) return map.get(key).get(timestamp);
-        else{
-            TreeMap<Integer, String> interMap = map.get(key);
-            for(Integer timestamp_prev : interMap.keySet()){
-                if(timestamp > timestamp_prev) return interMap.get(timestamp_prev);
-            }
-        }
-        */ 
     }
 }
 
