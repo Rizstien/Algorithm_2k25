0efbe7132a73b7857d34a5653b7eaa0a17a68909
Rizwan Munir <rizstien@gmail.com>
2026-01-18 16:31:04 +0500

    Time: 0 ms (100%), Space: 43.8 MB (52.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -9,32 +9,21 @@ class Solution {
         int right = nums.length-1;
         
         while(left<=right){
-            int mid = left+(right-left)/2;
-
-            if(nums[mid]==target) return mid;
+            int mid = (left+right)/2;
+            if(nums[mid] == target) return mid;
             if(nums[left]<=nums[mid]){
-                if(target<nums[left] || target>nums[mid]){
-                    left = mid+1;
+                if(target < nums[left] || target > nums[mid]){
+                    left = mid + 1;
                 }else{
-                    right = mid-1;
+                    right = mid - 1;
                 }
             }else{
-                if(target>nums[right] || target<nums[mid]){
-                    right = mid-1;
+                if(target > nums[right] || target < nums[mid]){
+                    right = mid - 1;
                 }else{
-                    left = mid+1;
+                    left = mid + 1;
                 }
             }
-        }
-        return -1;
-    }
-
-    public int binarySearch(int[] nums, int left, int right, int target){
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target) return mid;
-            if(target>nums[left] && target<nums[mid]) right = mid - 1;
-            else left = mid + 1;
         }
         return -1;
     }
