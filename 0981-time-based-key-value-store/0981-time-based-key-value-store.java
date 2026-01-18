bc2007e467950ab45fedd5eb56007adb6cd6b764
Rizwan Munir <rizstien@gmail.com>
2026-01-18 20:07:05 +0500

    Time: 152 ms (33.73%), Space: 108.6 MB (49.68%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -2,10 +2,11 @@ class TimeMap {
 
     Map<String, TreeMap<Integer, String>> map = null;
     public TimeMap() {
-        map = new HashMap<String, TreeMap<Integer, String>>();
+        map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
+        /*
         if(map.get(key) != null){
             TreeMap<Integer, String> interMap = map.get(key);
             interMap.put(timestamp, value);
@@ -14,20 +15,29 @@ class TimeMap {
             interMap.put(timestamp,value);
             map.put(key,interMap);
         }
+        */
+
+        map.computeIfAbsent(key, k-> new TreeMap<>()).put(timestamp,value);
+
     }
     
     public String get(String key, int timestamp) {
-        if(map.get(key) == null) return "";
+        TreeMap<Integer, String> treeMap = map.get(key);
+        if(treeMap == null) return "";
+        
+        Map.Entry<Integer, String> entry = treeMap.floorEntry(timestamp);
+
+        return entry == null ? "" : entry.getValue();
+
+        /*
+        if(map.get(key).get(timestamp) != null) return map.get(key).get(timestamp);
         else{
-            if(map.get(key).get(timestamp) != null) return map.get(key).get(timestamp);
-            else{
-                TreeMap<Integer, String> interMap = map.get(key);
-                for(Integer timestamp_prev : interMap.keySet()){
-                    if(timestamp > timestamp_prev) return interMap.get(timestamp_prev);
-                }
-            } 
+            TreeMap<Integer, String> interMap = map.get(key);
+            for(Integer timestamp_prev : interMap.keySet()){
+                if(timestamp > timestamp_prev) return interMap.get(timestamp_prev);
+            }
         }
-        return "";
+        */ 
     }
 }
 
